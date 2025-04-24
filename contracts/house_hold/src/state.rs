c82d49fb6c7fbd711d5c8ce3f5a4985a5953fb8b
Slymewll <slymewll@gmail.com>
2025-04-24 23:32:43 +0700

    add house hold contract

---
+++ contracts/house_hold/src/state.rs
@@ -0,0 +1,100 @@
+use soroban_sdk::{contracttype, vec, Address, Env, Vec};
+
+#[derive(Clone, Debug)]
+#[contracttype]
+pub struct Batch {
+    pub amount_per_beneficiary: u128,
+    pub reduced_amount_per_beneficiary: u128,
+}
+
+#[derive(Clone, Debug)]
+#[contracttype]
+pub enum StorageKey {
+    Initialized,
+    Admin,
+    IsContractPaused,
+    UsdAddress,
+    // batch keyed by batch_id
+    Batches(u128),
+    // paid_addresses keyed by batch_id
+    PaidAddresses(u128, Address),
+}
+
+pub fn set_initialized(e: &Env, initialized: &bool) {
+    e.storage().instance().set(&StorageKey::Initialized, initialized);
+}
+
+pub fn get_initialized(e: &Env) -> bool {
+    e.storage().instance().get::<_, bool>(&StorageKey::Initialized).unwrap_or(false)
+}
+
+pub fn set_admin(e: &Env, new_admin: &Address) {
+    e.storage().instance().set(&StorageKey::Admin, new_admin);
+}
+
+/// Return the admin address.
+pub fn get_admin(e: &Env) -> Address {
+    e.storage()
+        .instance()
+        .get::<_, Address>(&StorageKey::Admin)
+        .expect("Admin not set")
+}
+
+pub fn set_is_contract_paused(e: &Env, is_paused: &bool) {
+    e.storage()
+        .instance()
+        .set(&StorageKey::IsContractPaused, is_paused);
+}
+
+pub fn get_is_contract_paused(e: &Env) -> bool {
+    e.storage()
+        .instance()
+        .get::<_, bool>(&StorageKey::IsContractPaused)
+        .unwrap_or(false)
+}
+
+pub fn set_usd_address(e: &Env, usd_address: &Address) {
+    e.storage()
+        .instance()
+        .set(&StorageKey::UsdAddress, usd_address);
+}
+
+pub fn get_usd_address(e: &Env) -> Address {
+    e.storage()
+        .instance()
+        .get::<_, Address>(&StorageKey::UsdAddress)
+        .expect("UsdAddress not set")
+}
+
+pub fn set_batch(e: &Env, batch_id: &u128, batch: &Batch) {
+    e.storage()
+        .instance()
+        .set(&StorageKey::Batches(batch_id.clone()), batch);
+}
+
+pub fn get_batch(e: &Env, batch_id: &u128) -> Option<Batch> {
+    e.storage()
+        .instance()
+        .get::<_, Batch>(&StorageKey::Batches(batch_id.clone()))
+}
+
+pub fn set_paid_address(e: &Env, batch_id: &u128, address: &Address) {
+    e.storage()
+        .instance()
+        .set(&StorageKey::PaidAddresses(batch_id.clone(), address.clone()), &true);
+}
+
+pub fn get_paid_address(e: &Env, batch_id: &u128, address: &Address) -> bool {
+    e.storage()
+        .instance()
+        .get::<_, bool>(&StorageKey::PaidAddresses(batch_id.clone(), address.clone()))
+        .unwrap_or(false)
+}
+
+pub fn get_paid_addresses(e: &Env, batch_id: &u128, addresses: &Vec<Address>) -> Vec<bool> {
+    let mut paid_addresses = vec![&e];
+    for address in addresses {
+        paid_addresses.push_back(get_paid_address(e, &batch_id, &address));
+    }
+    paid_addresses
+}
